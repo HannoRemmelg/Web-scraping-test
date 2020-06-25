f984a392aacc5fb1a48f1a48f4d8bb0bb65b977c
Hanno Remmelg <67294298+HannoRemmelg@users.noreply.github.com>
2020-06-25 23:49:09 +0300

    Add files via upload

---
+++ newegg.com_gpudesilehelt_(toode,hind, shipping).py
@@ -0,0 +1,26 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as BSoup
+
+target_url = "https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%20card"
+uClient = urlopen(target_url)
+page_raw_html = uClient.read()
+uClient.close()
+
+page_soup = BSoup(page_raw_html, "html.parser")
+containers = page_soup.findAll("div", {"class": "item-container"})
+
+
+for container in containers:
+    #brand = container.div.div.a.img["title"]
+    brand_container = container.findAll("a", {"class": "item-brand"})
+    product_brand = brand_container[0].img["alt"]
+
+    title_container = container.findAll("a", {"class": "item-title"})
+    product_name = title_container[0].text
+
+    price_ship = container.findAll("li", {"class": "price-ship"})
+    product_shipping = price_ship[0].text.strip()
+
+    print("brand: ", product_brand)
+    print("product_name: ", product_name)
+    print("product_shipping: ", product_shipping)
